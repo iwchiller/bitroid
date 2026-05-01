ab28f727b161b76b35f8617c78a8ffcec34d1859
IW Chiller <iw_chiller@mail.ru>
2026-05-02 00:39:34 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,96 @@
+// ------------------------------------------------------------ //
+// Видимость каруселей для мобильных и десктопных версий сайта
+// ------------------------------------------------------------ //
+
+
+var sliderGallery = function() {
+
+	/*** Vars ***/
+	var gallery1 = '.recommendations_items',
+		gallery2 = '.stacks',
+		slider = false;
+
+	/*** Init ***/
+	var init = function() {
+		manage(); // On load (1*)
+
+		$(window).on('resize', function(){ // On resize (2*)
+			waitForFinalEvent(function(){
+				manage();
+			}, 200, "sliderGallery");
+		});
+
+	};
+
+	/*** Manage slider ***/
+	var manage = function() {
+		var isMobile = $('#bp_mobile').is(':visible');
+		var isDesktop = $('#bp_desktop').is(':visible');
+		var isCloseIcon = $('.icon_close').is(':visible');
+
+		if( isMobile && !slider ) { // If mobile and slider not built yet = build
+			build();
+		}
+		else if( !isMobile && slider ) { // Not mobile but slider built = destroy
+			destroy();
+		}
+
+		if (isDesktop && !isCloseIcon) {
+			$('.menu_block').css('display','flex');
+		}
+
+	};
+
+	/*** Build slider ***/
+	var build = function() {
+		slider1 = $(gallery1).addClass('owl-carousel'); // Add owl slider class (3*)
+		slider1.owlCarousel({ // Initialize slider
+			items:1,
+            margin: 0,
+            loop: false,
+            dots: true
+		});
+
+		slider2 = $(gallery2).addClass('owl-carousel'); // Add owl slider class (3*)
+		slider2.owlCarousel({ // Initialize slider
+			items:1,
+            margin: 0,
+			autoHeight: true,
+            loop: false,
+            dots: true
+		});
+	};
+
+	/*** Destroy slider ***/
+	var destroy = function() {
+		slider1.trigger('destroy.owl.carousel'); // Trigger destroy event (4*)
+		slider1 = false; // Reinit slider variable
+		$(gallery1).removeClass('owl-carousel'); // Remove owl slider class (3*)
+
+		slider2.trigger('destroy.owl.carousel'); // Trigger destroy event (4*)
+		slider2 = false; // Reinit slider variable
+		$(gallery2).removeClass('owl-carousel'); // Remove owl slider class (3*)
+	};	
+
+	/*** Public methods***/
+	return {
+		init: init
+	};
+
+}();
+
+// ---------------------------------------------------- //
+// PREVENT MULTIPLE CALLS
+// ---------------------------------------------------- //
+var waitForFinalEvent = (function () {
+	var timers = {};
+	return function (callback, ms, uniqueId) {
+		if (!uniqueId) {
+			uniqueId = "Don't call this twice without a uniqueId";
+		}
+		if (timers[uniqueId]) {
+			clearTimeout (timers[uniqueId]);
+		}
+		timers[uniqueId] = setTimeout(callback, ms);
+	};
+})();
---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+sliderGallery.init();
+
+// Карусель кейов    
+    $('.cases_carousel').owlCarousel({
+        items:3,
+        responsive:{
+            360: {
+                items:1
+            },
+            768: {
+                items:2
+            },
+            1440:{
+                items:3
+            }},        
+        margin: 0,
+        loop:true,
+        dots: true,
+        nav:true,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        autoplay:false,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+    
+// FAQ аккордеон
+    $('.question > .header').click(function() {
+        if($(this).children('.icon').text() === '+') {
+            $(this).parent().addClass('question_expand');
+            $(this).children('.title').addClass('question_title_expand');
+            $(this).children('.icon').addClass('question_icon_expand');
+            $(this).children('.icon').text('—');
+        } else {
+            $(this).parent().removeClass('question_expand');
+            $(this).children('.title').removeClass('question_title_expand');
+            $(this).children('.icon').removeClass('question_icon_expand');
+            $(this).children('.icon').text('+');
+        };
+        $(this).next().toggle('fast');
+    });
+
+    $('.icon_menu').click(function() {
+        $('.menu_block').css('display', 'flex');
+        $('.icon_close').css('display', 'block');
+    });
+
+    $('.icon_close').click(function() {
+        $('.menu_block').css('display', 'none');
+        $('.icon_close').css('display', 'none');
+    });
+
+
+    
+})
